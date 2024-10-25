957311fcce4978a99dc67062a4bda3a6d69d8786
AmirBl82 <amirshayanbolandzadeh@gmail.com>
2024-10-25 12:16:48 +0330

    AI_Python Codes

--- BFS_Frontier_Explored.py
+++ BFS_Frontier_Explored.py
@@ -1,30 +1,31 @@
 from collections import deque
 import networkx as nx
-def bfs_target(graph,start_node,target):
-    frontier = deque([start_node])
+
+def bfs_target(graph, start_node, target):
+    frontier = deque([(start_node, [start_node])])
     explored = set()
-    explored.add(start_node)
+    
     while frontier:
-        current_node = frontier.popleft()
+        current_node, path = frontier.popleft()
 
         if current_node == target:
-            return f"Target '{target} found"
+            return f"Target '{target}' found, path: {'->'.join(path)}"
         else:
             for neighbor in graph.neighbors(current_node):
-                explored.add(neighbor)
-                frontier.append(neighbor)
+                if neighbor not in explored:
+                    explored.add(neighbor)
+                    new_path = path + [neighbor]
+                    frontier.append((neighbor, new_path))
                 
-    
     return f"Target '{target}' not found"
-    
 
 Graph = nx.Graph()
 edges = [
-    ('A','B'), ('A','C'),
-    ('B','A'), ('B', 'D'),
-    ('C','B'), ('C','F'),
+    ('A', 'B'), ('A', 'C'),
+    ('B', 'A'), ('B', 'D'),
+    ('C', 'B'), ('C', 'F'),
 ]
 Graph.add_edges_from(edges)
 
-print(bfs_target(Graph,'A','F'))
-print(bfs_target(Graph,'A','G'))
+print(bfs_target(Graph, 'A', 'F'))
+print(bfs_target(Graph, 'A', 'G'))
