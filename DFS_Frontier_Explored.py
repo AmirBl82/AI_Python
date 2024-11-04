cdc478e0f1a8fc5281af17f08949c0c5c483b451
AmirBl82 <98527218+AmirBl82@users.noreply.github.com>
2024-11-04 14:24:28 +0330

    AI_Python Codes

--- DFS_Frontier_Explored.py
+++ DFS_Frontier_Explored.py
@@ -1,14 +1,15 @@
 import networkx as nx
-from collections import deque
-stack = deque()
+from queue import LifoQueue
+
+stack = LifoQueue()
 
 def dfs_target(graph, start_node, target):
     frontier = stack
-    frontier.append((start_node,[start_node])) 
+    frontier.put((start_node, [start_node]))
     explored = set()
 
-    while frontier:
-        current_node, path = frontier.pop()
+    while not frontier.empty():
+        current_node, path = frontier.get()
 
         if current_node == target:
             return f"Target {target} found, path: {'->'.join(path)}"
@@ -19,14 +20,14 @@ def dfs_target(graph, start_node, target):
             for neighbor in graph.neighbors(current_node):
                 if neighbor not in explored:
                     new_path = path + [neighbor]
-                    frontier.append((neighbor, new_path))
+                    frontier.put((neighbor, new_path))
     
     return f"Target {target} not found"
 
 Graph = nx.Graph()
-edges = [('A','B'),('A','C'),
-         ('B','D'),('B','C'),('B','E'),
-         ('C','D'),('C','E'),('D','E')]
+edges = [('A', 'B'), ('A', 'C'),
+         ('B', 'D'), ('B', 'C'), ('B', 'E'),
+         ('C', 'D'), ('C', 'E'), ('D', 'E')]
 Graph.add_edges_from(edges)
 
-print(dfs_target(Graph,'A','D'))
+print(dfs_target(Graph, 'A', 'D'))
